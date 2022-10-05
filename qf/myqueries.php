d4b260d598c20bacedb4a490793511d33ade781a
Achraf S <42727296+sendidach@users.noreply.github.com>
2022-10-05 18:42:23 +0100

    7

--- qf/myqueries.php
+++ qf/myqueries.php
@@ -1,30 +1,17 @@
 <?php
 //require_once($_SERVER['DOCUMENT_ROOT'] . "/includes/connect.php");
-require_once($_SERVER['DOCUMENT_ROOT'] . "/functions/functions.php");
-
+// require_once($_SERVER['DOCUMENT_ROOT'] . "/functions/functions.php");
 $response = [];
-$data = [];
 
-if (isset($_POST['action']) && !empty($_POST['action'])) {
+if (isset($_POST)){
+   $action =  $_POST['action'];
+   $inputvalue = $_POST['inputvalue'];
 
-    $action = $_POST['action'];
+   $inputvalue = strtoupper($inputvalue);
+   
+    $response['action'] = $action;
+    $response['inputvalue'] = $inputvalue;
 
-    if ($action == 'loadUsers') {
-
-        $response['data'] = 'Positive';
-        $response['message'] = 'SUCCESS';
-        $response['status'] = 200;
-
-    } else {
-        $response['data'] = 'Error 1';
-        $response['message'] = 'Veuillez verifier votre requete';
-        $response['status'] = 206;
-    }
-} else {
-
-    $response['data'] = 'error 0';
-    $response['message'] = 'requete incomplete, contactez votre administrateur avec le code suivant : P_ACTION 321F45';
-    $response['status'] = 404;
+    
+   echo json_encode($response);
 }
-
-echo json_encode($response);
