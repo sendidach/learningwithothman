97bb8af25c7403d05e18f74c5243ddd64e966318
Achraf S <42727296+sendidach@users.noreply.github.com>
2022-10-14 13:39:23 +0100

    Step 2

--- qf/myqueries.php
+++ qf/myqueries.php
@@ -2,16 +2,39 @@
 //require_once($_SERVER['DOCUMENT_ROOT'] . "/includes/connect.php");
 // require_once($_SERVER['DOCUMENT_ROOT'] . "/functions/functions.php");
 $response = [];
+$data = [];
+$message = '';
 
-if (isset($_POST)){
+if (isset($_POST['action']) && !empty($_POST['action'])) {
+
    $action =  $_POST['action'];
-   $inputvalue = $_POST['inputvalue'];
+   if ($action == 'getUsername') {
+      $inputvalue = $_POST['inputvalue'];
+      $inputvalue = strtoupper($inputvalue);
+      $data = $inputvalue;
+      $message = 'YOOPIIII!';
+      $response['statut'] = 200;
+      $response['data'] = $data;
+      $response['message'] = $message;
 
-   $inputvalue = strtoupper($inputvalue);
-   
-    $response['action'] = $action;
-    $response['inputvalue'] = $inputvalue;
+   } elseif ($action == 'getphoneNumber') {
+      $inputvalue = $_POST['inputvalue'];
+      $data = $inputvalue;
+      $message = 'YOOHOOO!';
+      $response['statut'] = 200;
+      $response['data'] = $data;
+      $response['message'] = $message;
 
-    
-   echo json_encode($response);
+   } else { 
+      $message = 'This action doesn\'t exist';
+      $response['statut'] = 204;
+      $response['message'] = $message;
+   }
+
+} else {
+   $message = 'This method is incorrect';
+   $response['statut'] = 206;
+   $response['message'] = $message;
 }
+
+echo json_encode($response);
