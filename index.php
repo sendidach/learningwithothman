e75ab08ed2ae11f1c5b89ea5c87b18a9626a6a29
Achraf S <42727296+sendidach@users.noreply.github.com>
2022-10-17 01:24:18 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,23 +2,19 @@
 $user = 'maxmind_sendidach';
 $pass = '4042Juice';
 
+// $user = 'sendidach';
+// $pass = '4042Juice!';
+
 
 
 try{
-   $db = new PDO ('mysql:host=localhost;maxmind_achraf',$user,$pass);
+   $db = new PDO ('mysql:host=127.0.0.1;maxmind_achraf',$user,$pass);
    echo '<h1>Succesful Connection to the DB</h1>';
-   $query = 'SELECT * FROM tb_users';
-   $statement = $db->prepare($query);
-   $statement->execute();
-   $results = $statement->fetchAll();
-   $statement->closeCursor();      
-
-    foreach($results as $r){
-            echo $r['firstname'] . '<br />';
-    }
-
+   foreach ($db->query('SELECT * FROM tb_users') as $row) {
+      print_r($row);
+   }
 }
 catch (PDOException $e){
-   print "Erreur " . $e->getMessage() . "<br/>";
+   print "Erreur: " . $e->getMessage() . "<br/>";
    die;
 }
